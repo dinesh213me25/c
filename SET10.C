3eaf50d0e4065173b5d515984b22b2885223b5fa
dinesh213me25 <35915036+dinesh213me25@users.noreply.github.com>
2018-02-04 14:02:36 +0530

    Create SET10.C

---
+++ SET10.C
@@ -0,0 +1,22 @@
+Idhaya Kumar <idhaya34u@gmail.com>
+1:57 PM (5 minutes ago)
+
+to me 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n,k,sum=0;
+printf("enter the integers:");
+scanf("%d%d",&n,&k);
+for(i=1;i<=n;i++)
+{
+printf("%d",i);
+}
+for(j=1;j<=k;j++)
+{
+sum=sum+j;
+}
+printf("the sum of %d integers:",sum);
+getch();
+}
