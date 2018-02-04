ceec9d90771116d77cb50e34131739a80e19292a
dinesh213me25 <35915036+dinesh213me25@users.noreply.github.com>
2018-02-04 09:22:02 +0530

    Create + or -ve.c

---
+++ + or -ve.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the Number");
+scanf("%d",&a);
+if(a>0)
+printf("The Number is positive");
+else(a<0)
+printf("The Number is Negative");
+else
+printf("The Number is zero");
+return 0;
+}
