c114242b286a778aae52b01f212d84ed663cb37b
dinesh213me25 <35915036+dinesh213me25@users.noreply.github.com>
2018-02-09 16:06:16 +0530

    Create find sum of arithmetic profersion.c

---
+++ find sum of arithmetic profersion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,f1=0,f2=1,fibo;
+    scanf("%d",&n);
+    printf("%d\n%d\n",f1,f2);
+    int i;
+    for(i=2;i<n;i++)
+    {
+        fibo=f1+f2;
+        printf("%d\n",fibo);
+        f1=f2;
+        f2=fibo;
+        }
+        return 0;
+        getch();
+        }
