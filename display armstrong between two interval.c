1f5ab497358385c20a342b3bad1d2d002d1e99d3
dinesh213me25 <35915036+dinesh213me25@users.noreply.github.com>
2018-02-09 15:53:51 +0530

    Create display armstrong between two interval.c

---
+++ display armstrong between two interval.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) {
+int a,b,temp,result=0,rem;
+scanf("%d%d",&a,&b);
+for(int i=a+1;i<b;i++)
+{
+temp=i;
+while(temp!=0)
+{
+	rem=temp%10;
+	result+=rem*rem*rem;
+	temp=temp/10;
+}
+if(i==result)
+{
+	printf("%d\n",i);
+}
+}
+	return 0;
+  getch();
+}
