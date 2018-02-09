7e70ef6f3115fcf2271359c0d3390c5b49a62137
dinesh213me25 <35915036+dinesh213me25@users.noreply.github.com>
2018-02-09 16:29:45 +0530

    Create set24.c

---
+++ set24.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+	int i,a[100],n,temp=0,j;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d\n",a[i]);
+	}
+  getch();
+}
