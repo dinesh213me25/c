275c1d39c1c10017747757dff1887b4566eea497
dinesh213me25 <35915036+dinesh213me25@users.noreply.github.com>
2018-02-19 13:23:12 +0530

    Create set31.c

---
+++ set31.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<conio.h>
+int main()
+{
+int a;
+char ch[100];
+printf("enter the sentence..");
+scanf("%s",&ch);
+printf("\n %d",strlen(ch));
+return 0;
+}
