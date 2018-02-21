597c4de32a87970d62a8c9b1ca0939be2d50916c
dinesh213me25 <35915036+dinesh213me25@users.noreply.github.com>
2018-02-21 04:08:06 -0500

    Create set49.c

---
+++ set49.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n1,n2,sum;
+printf("enter the number:\n");
+scanf("%d",&n1);
+printf("enter the number:\n");
+scanf("%d",&n2);
+sum=n1+n2;
+printf("%d",sum);
+return 0;
+getch();
+}
