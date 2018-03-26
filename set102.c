5890b8dab5cd408a7362c41311fea3b48a90b88a
dinesh213me25 <35915036+dinesh213me25@users.noreply.github.com>
2018-03-26 15:53:45 +0530

    Create set102.c

---
+++ set102.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include<conio.h>
+
+int main(void) 
+{
+	int num,i;
+	scanf("%d",&num);
+	int num1,rem;
+	while(num!=1)
+	{
+	rem=num%2;
+	num1=num/2;
+	printf("%d\n",num1);
+	num=rem;
+	break;
+	}
+	return 0;
+  getch();
+}
