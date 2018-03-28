dc52a903fa7c8245a8cf88c656db9aff358b3a8d
dinesh213me25 <35915036+dinesh213me25@users.noreply.github.com>
2018-03-28 15:48:08 +0530

    Create set107.c

---
+++ set107.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include<conio.h>
+
+int main(void) 
+{
+	int a,b,c,result;
+	scanf("%d %d %d",&a,&b,&c);
+	result=((a*b)/c);
+	printf("%d",result);
+	return 0;
+  getch();
+}
